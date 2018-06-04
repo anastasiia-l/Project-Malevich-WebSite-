a5f62e6b066b1e6b731b1d7452350c1117fd7001
Anastasiia <anastasiia.leshkevych@nure.ua>
2018-06-04 03:06:53 +0300

    Add itemlist

---
+++ js/menu.js
@@ -0,0 +1,62 @@
+$document.ready(function(){
+	loadGoods();
+});
+
+function loadGoods() {
+var requestURL = 'https://malevich-server.herokuapp.com/menu/all';
+var request = new XMLHttpRequest();
+setCookie('sid','sid');
+request.open('GET', requestURL);
+request.responseType = 'json';
+xhr.withCredentials = true;
+request.send();
+request.onload = function() {
+  var superHeroes = request.response;
+  console.log(superHeroes);
+  populateHeader(superHeroes);
+  
+}
+}
+
+function populateHeader(jsonObj) {
+	var out = '';
+	for (var key in jsonObj) {
+		out += '<div class="dish-card">' + '<img class="dish-img" src="' + jsonObj[key]['imageURL'] + '">';
+		out += '<p class="dish-title">' + jsonObj[key]['name'] + '</p>';
+		out += '<div class="dish-description"><p class="dish-description">' + jsonObj[key]['description'] + '</p></div>';
+		out += '<p class="dish-cost">' + jsonObj[key]['price'] + '<p>';
+		out += '<div class="order-button">Заказать</div></div>';
+
+	}
+  	$('#goods').html(out);		
+				
+}
+
+function setCookie(name, value, options) {
+  options = options || {};
+
+  var expires = options.expires;
+
+  if (typeof expires == "number" && expires) {
+    var d = new Date();
+    d.setTime(d.getTime() + expires * 1000);
+    expires = options.expires = d;
+  }
+  if (expires && expires.toUTCString) {
+    options.expires = expires.toUTCString();
+  }
+
+  value = encodeURIComponent(value);
+
+  var updatedCookie = name + "=" + value;
+
+  for (var propName in options) {
+    updatedCookie += "; " + propName;
+    var propValue = options[propName];
+    if (propValue !== true) {
+      updatedCookie += "=" + propValue;
+    }
+  }
+
+  document.cookie = updatedCookie;
+}
