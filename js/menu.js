1207f2c799ceafbe0825c977c56633a6af714d9e
Anastasiia <anastasiia.leshkevych@nure.ua>
2018-06-04 20:05:43 +0300

    Add createCORSRequest

--- js/menu.js
+++ js/menu.js
@@ -5,10 +5,30 @@ $('document').ready(function(){
 function loadGoods() {
 var requestOpenSession = "https://malevich-server.herokuapp.com/session/start";
 
+  var xhr = createCORSRequest('GET', requestOpenSession);
+  if (!xhr) {
+    alert('CORS not supported');
+    return;
+  }
+
+  // Response handlers.
+  xhr.onload = function() {
+    var text = xhr.responseText;
+    var title = getTitle(text);
+    alert('Response from CORS request to ' + url + ': ' + title);
+  };
+
+  xhr.onerror = function() {
+    alert('Woops, there was an error making the request.');
+  };
+
+  xhr.send();
+/*
 var request = new XMLHttpRequest();
 request.open('GET', requestOpenSession);
 request.send();
 console.log(request.response);
+*/
 
 var requestMenuURL = 'https://malevich-server.herokuapp.com/menu/all';
 
@@ -66,4 +86,31 @@ function setCookie(name, value, options) {
   }
 
   document.cookie = updatedCookie;
+}
+
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+    // XHR for Chrome/Firefox/Opera/Safari.
+    xhr.open(method, url, true);
+  } else if (typeof XDomainRequest != "undefined") {
+    // XDomainRequest for IE.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+  } else {
+    // CORS not supported.
+    xhr = null;
+  }
+  return xhr;
+}
+
+// Helper method to parse the title tag from the response.
+function getTitle(text) {
+  return text.match('<title>(.*)?</title>')[1];
+}
+
+// Make the actual CORS request.
+function makeCorsRequest() {
+  // This is a sample server that supports CORS.
+ 
 }
