8449f5d471cef933330a8aa5b3116d3fb86448a7
Unknown <vadim.feoklistov12@gmail.com>
2018-06-05 17:32:23 +0300

    add fixed header
    and fixed icon instagram

---
+++ js/fixed_header.js
@@ -0,0 +1,9 @@
+$(document).scroll(function () {
+    if ($(this).scrollTop() >= 160) { 
+        $('.fixed').addClass('sticky');
+    } else {
+        $('.fixed').removeClass('sticky');
+    }
+});
+
+
